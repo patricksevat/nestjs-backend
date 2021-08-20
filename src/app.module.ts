b7959103977f341bb5fae258f3d04d1cbe825ac3
patricksevat <patrick.sevat@rabobank.nl>
2021-08-20 13:45:54 +0200

    chore(): clean up unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { EventModule } from './event/event.module';
 
@@ -20,7 +18,7 @@ import { EventModule } from './event/event.module';
     UserModule,
     EventModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
