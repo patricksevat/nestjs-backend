83d494eff49dd55eb5bbc376a7a04191b5695e49
patricksevat <patrick.sevat@rabobank.nl>
2021-08-24 18:06:23 +0200

    fix(): remove active property from response

--- src/user/interceptors/events-to-consent.interceptor.ts
+++ src/user/interceptors/events-to-consent.interceptor.ts
@@ -6,7 +6,8 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { IUserResponse, User } from '../entities/user.entity';
+import { IConsent, User } from '../entities/user.entity';
+import { EventEntity } from '../../event/entities/event.entity';
 
 @Injectable()
 export class EventsToConsentInterceptor implements NestInterceptor {
@@ -14,17 +15,39 @@ export class EventsToConsentInterceptor implements NestInterceptor {
     return next.handle().pipe(
       map((value) => {
         if (value instanceof User) {
-          if (!value.events) {
-            return {
-              id: value.id,
-              email: value.email,
-              consents: [],
-            };
-          }
+          return {
+            id: value.id,
+            email: value.email,
+            consents: this.getConsents(value?.events?.[0]),
+          };
         }
 
         return value;
       }),
     );
+  }
+
+  getConsents(activeEvent: EventEntity): IConsent[] {
+    const consents: IConsent[] = [];
+
+    if (!activeEvent) {
+      return [];
+    }
+
+    if (activeEvent.sms_notifications) {
+      consents.push({
+        id: 'sms_notifications',
+        enabled: true,
+      });
+    }
+
+    if (activeEvent.email_notifications) {
+      consents.push({
+        id: 'email_notifications',
+        enabled: true,
+      });
+    }
+
+    return consents;
   }
 }
